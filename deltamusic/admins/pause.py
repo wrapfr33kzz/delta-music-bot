b6a425303c71de55042221c910936140d8d49713
Robii <92801274+wrapfr33kzz@users.noreply.github.com>
2022-05-15 22:20:21 +0530

    Update pause.py

--- deltamusic/admins/pause.py
+++ deltamusic/admins/pause.py
@@ -13,7 +13,7 @@ from pyrogram.types import Message
 from config import BANNED_USERS
 from strings import get_command
 from deltamusic import app
-from deltamusic.core.call import Yukki
+from deltamusic.core.call import delta
 from deltamusic.utils.database import is_music_playing, music_off
 from deltamusic.utils.decorators import AdminRightsCheck
 
@@ -34,7 +34,7 @@ async def pause_admin(cli, message: Message, _, chat_id):
     if not await is_music_playing(chat_id):
         return await message.reply_text(_["admin_1"])
     await music_off(chat_id)
-    await Yukki.pause_stream(chat_id)
+    await delta.pause_stream(chat_id)
     await message.reply_text(
         _["admin_2"].format(message.from_user.mention)
     )
